b4fa9b5cdec36a42ac067b93d9cdb7e1bd770100
balumanobiram <100069649+balumanobiram@users.noreply.github.com>
2023-11-21 23:15:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -299,10 +299,10 @@ app.post("/userprofile", async (req, res) => {
 
 app.post("/ucanceltickets", async (req, res) => {
     const { username,
-        selectedEvent} = req.body
+        selectedEvent,booked} = req.body
 
     try {
-        const check = await usereventsconn.deleteOne({username:username,eventname:selectedEvent})
+        const check = await usereventsconn.deleteOne({username:username,eventname:selectedEvent,booked:booked})
 
         if (check) {
             res.json("exist")
